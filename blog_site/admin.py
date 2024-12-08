3911499b713651571fbf742ffdef638d373adf4a
TulqinWeb <tulqinurinov005@gmail.com>
2024-12-08 21:26:47 +0500

    added model and admin

--- blog_site/admin.py
+++ blog_site/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog_site.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')  # Postlar ruyxati ko'rinadi
+    list_filter = ('status', 'created', 'publish', 'author')  # filterlash
+    search_fields = ('title', 'body')  # qidirish uchun
+    prepopulated_fields = {'slug':('title',)}  # title ga qarab o'zi yaratadi
+    raw_id_fields = ('author',) # admin panelda ko'rinadi
+    date_hierarchy = 'publish' # ro'yxatni ko'rsatilosh tartibi
+    ordering = ('status', 'publish') # status publish bo'lganlar yuqorida turishi uchun
+
+
+
+
